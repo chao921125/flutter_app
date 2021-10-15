75c2374324572f06d07a32eb37ea0a076644a34e
黄超 <chao.huang@advokategroup.com>
2021-10-15 15:46:32 +0800

    add

--- Puppeteer/demo/demo.js
+++ Puppeteer/demo/demo.js
@@ -7,9 +7,5 @@ const puppeteer = require('puppeteer');
  * 启动入口
  */
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-    // other actions...
-    // await browser.close();
-  })();
+  
+})();
