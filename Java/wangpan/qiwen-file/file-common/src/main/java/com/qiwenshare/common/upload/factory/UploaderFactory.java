457aa4bcc892b3fbd9c8298a27a7eebab275276e
黄超 <chao.huang@advokategroup.com>
2021-10-25 18:09:24 +0800

    ad

--- Java/wangpan/qiwen-file/file-common/src/main/java/com/qiwenshare/common/upload/factory/UploaderFactory.java
+++
@@ -1,9 +0,0 @@
-package com.qiwenshare.common.upload.factory;
-
-import com.qiwenshare.common.domain.UploadFile;
-import com.qiwenshare.common.upload.Uploader;
-
-public interface UploaderFactory {
-    public Uploader getUploader();
-    public Uploader getUploader(UploadFile uploadFile);
-}
--- Java/wangpan/qiwen-file/file-web/src/main/java/com/qiwenshare/file/dto/user/RegisterDTO.java
+++
@@ -1,6 +0,0 @@
-package com.qiwenshare.file.dto.user;
-
-
-public class RegisterDTO {
-
-}
