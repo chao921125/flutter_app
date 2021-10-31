7db569495c3364def3d3ba08299dc71fdea4a383
hc precog <chao921125@sina.com>
2021-10-31 23:30:57 +0800

    add

---
+++ Puppeteer/sex-ty66/address.js
@@ -0,0 +1,5 @@
+// https://t66y.com/thread0806.php?fid=25&search=&page=2
+// https://t66y.com/htm_data/2110/25/4761642.html
+// document.querySelector("#main div.tpc_content > a").href;
+// document.querySelector("form button[type=submit]").click();
+let url = "https://t66y.com/thread0806.php?fid=25&search=&page=1";
