93c88d23f2d24e99e26e7420dc26bf65584fc2f7
hc precog <chao921125@sina.com>
2021-12-11 12:23:51 +0800

    update

--- Puppeteer/sex-1024/heji.js
+++ Puppeteer/sex-1024/heji.js
@@ -28,7 +28,7 @@ const optionsLaunch = {
 
 // 12-08
 let pageUrl = "https://m2.5y1rsxmzh.com/pw/thread.php?fid=3";
-let pageSize = 63;
+let pageSize = 305;
 let isClose = false;
 
 const initBrowser = async () => {
@@ -37,7 +37,7 @@ const initBrowser = async () => {
     // });
     const browser = await puppeteer.launch(optionsLaunch);
     const page = await browser.newPage();
-    for (let i = 28; i <= pageSize; i++) {
+    for (let i = 1; i <= pageSize; i++) {
         console.log(i);
         await page.goto(pageUrl + `&page=${i}`);
         await getData(page, browser, i);
@@ -47,8 +47,6 @@ const initBrowser = async () => {
 
 const getData = async (page, browser, index) => {
     await page.waitForSelector("#ajaxtable");
-    // let divArray = await page.$$("body > div.main > div.left > div.gushici");
-    // 
     let listLength = await page.$$eval("#ajaxtable > tbody:nth-child(2) > tr.tr3", el => el.length);
     let start = 1;
     if (index === 1) {
@@ -57,19 +55,28 @@ const getData = async (page, browser, index) => {
     for (let i = start; i <= listLength; i++) {
         const pageDetail = await browser.newPage();
         try {
-            let linkHref = await page.$eval(`#ajaxtable > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(2) > h3 > a`, el => el.href);
-            console.log(linkHref);
-            // await pageDetail.goto(linkHref);
-            // let downHref = await pageDetail.$eval("#read_tpc > a", el => el.href);
-            // await pageDetail.goto(downHref);
-            // await pageDetail.click("body > div.tm-section.tm-section-color-1.tm-section-colored > div.uk-container.uk-container-center.uk-text-center.hashinfo > div.uk-width-medium-8-10.uk-width-1-1.uk-container-center.uk-text-center > div > div.uk-width-1-1.uk-text-center.dlboxbg > a:nth-child(1)");
-            // pageDetail.close();
-            // let pageSizeAll = await browser.pages();
-            // if (pageSizeAll.length > 4) {
-            //     for (let j = 3; j <= pageSizeAll.length; j++) {
-            //         pageSizeAll[j].close();
-            //     }
-            // }
+            let content = await page.$eval(`#ajaxtable > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(2) > h3 > a`, el => el.innerText);
+            if (content.includes("國產") || content.includes("国产")) {
+                let linkLength = await page.$$eval("#read_tpc > a", el => el.length);
+                for (let j = 1; j <= linkLength; j++) {
+                    let linkHref = await page.$eval(`#read_tpc > a:nth-child(${j})`, el => el.href);
+                    console.log(linkHref);
+                    // let linkHref = await page.$eval(`#ajaxtable > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(2) > h3 > a`, el => el.href);
+                    // await pageDetail.goto(linkHref);
+                    // let downHref = await pageDetail.$eval("#read_tpc > a", el => el.href);
+                    // await pageDetail.goto(downHref);
+                    // await pageDetail.click("body > div.tm-section.tm-section-color-1.tm-section-colored > div.uk-container.uk-container-center.uk-text-center.hashinfo > div.uk-width-medium-8-10.uk-width-1-1.uk-container-center.uk-text-center > div > div.uk-width-1-1.uk-text-center.dlboxbg > a:nth-child(1)");
+                    // pageDetail.close();
+                }
+                // let pageSizeAll = await browser.pages();
+                // if (pageSizeAll.length > 4) {
+                //     for (let j = 3; j <= pageSizeAll.length; j++) {
+                //         pageSizeAll[j].close();
+                //     }
+                // }
+            } else {
+                continue;
+            }
         } catch(e) {
             // pageDetail.close();
             continue;
