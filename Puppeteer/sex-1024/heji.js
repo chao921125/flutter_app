dbb747abe28b044c8174934a5efd1bd04230441f
hc precog <chao921125@sina.com>
2021-12-12 11:58:28 +0800

    update

--- Puppeteer/sex-1024/heji.js
+++ Puppeteer/sex-1024/heji.js
@@ -35,9 +35,9 @@ const initBrowser = async () => {
     // await page.evaluateOnNewDocument(() => {
     //     Object.defineProperty(navigator, 'webdriver', { get: () => false });
     // });
-    const browser = await puppeteer.launch(optionsLaunch);
-    const page = await browser.newPage();
     for (let i = 1; i <= pageSize; i++) {
+        const browser = await puppeteer.launch(optionsLaunch);
+        const page = await browser.newPage();
         console.log(i);
         await page.goto(pageUrl + `&page=${i}`);
         await getData(page, browser, i);
@@ -57,28 +57,28 @@ const getData = async (page, browser, index) => {
         try {
             let content = await page.$eval(`#ajaxtable > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(2) > h3 > a`, el => el.innerText);
             if (content.includes("國產") || content.includes("国产")) {
-                let linkLength = await page.$$eval("#read_tpc > a", el => el.length);
-                for (let j = 1; j <= linkLength; j++) {
-                    let linkHref = await page.$eval(`#read_tpc > a:nth-child(${j})`, el => el.href);
-                    console.log(linkHref);
-                    // let linkHref = await page.$eval(`#ajaxtable > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(2) > h3 > a`, el => el.href);
-                    // await pageDetail.goto(linkHref);
-                    // let downHref = await pageDetail.$eval("#read_tpc > a", el => el.href);
-                    // await pageDetail.goto(downHref);
-                    // await pageDetail.click("body > div.tm-section.tm-section-color-1.tm-section-colored > div.uk-container.uk-container-center.uk-text-center.hashinfo > div.uk-width-medium-8-10.uk-width-1-1.uk-container-center.uk-text-center > div > div.uk-width-1-1.uk-text-center.dlboxbg > a:nth-child(1)");
-                    // pageDetail.close();
+                let linkHref = await page.$eval(`#ajaxtable > tbody:nth-child(2) > tr:nth-child(${i}) > td:nth-child(2) > h3 > a`, el => el.href);
+                await pageDetail.goto(linkHref);
+                let downHrefArr = await pageDetail.$$eval("#read_tpc > a", el => {
+                    let hrefArr = [];
+                    for (let j = 0; j < el.length; j++) {
+                        hrefArr.push(el[j].getAttribute("href"));
+                    }
+                    return hrefArr;
+                });
+                for (const downHref of downHrefArr) {
+                    const pageDownload = await browser.newPage();
+                    await pageDownload.goto(downHref);
+                    await pageDownload.click("body > div.tm-section.tm-section-color-1.tm-section-colored > div.uk-container.uk-container-center.uk-text-center.hashinfo > div.uk-width-medium-8-10.uk-width-1-1.uk-container-center.uk-text-center > div > div.uk-width-1-1.uk-text-center.dlboxbg > a:nth-child(1)");
+                    await pageDownload.close();
                 }
-                // let pageSizeAll = await browser.pages();
-                // if (pageSizeAll.length > 4) {
-                //     for (let j = 3; j <= pageSizeAll.length; j++) {
-                //         pageSizeAll[j].close();
-                //     }
-                // }
+                await pageDetail.close();
             } else {
+                await pageDetail.close();
                 continue;
             }
         } catch(e) {
-            // pageDetail.close();
+            await pageDetail.close();
             continue;
         }
     }
