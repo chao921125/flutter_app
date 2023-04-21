44fd0835350011049aa54ea030982468ef166978
chao <chao921125@sina.com>
2023-04-21 16:40:24 +0800

    delete

--- Puppeteer/demo/test.js
+++
@@ -1,7 +0,0 @@
-const puppeteer = require('puppeteer');
-
-puppeteer.launch({headless: false, ignoreDefaultArgs: ["--enable-automation", "--enable-blink-features"]}).then(async browser => {
-  const page = await browser.newPage();
-  await page.goto('https://google.com');
-//   await browser.close();
-});
